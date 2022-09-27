e1f04b27974d9ac63c558eda2dff8f5f6df98e0f
Nicola Lerra <github.nicolalerra@gmail.com>
2022-09-27 18:21:46 +0200

    ESERCIZIO COMPLETATO css

--- assets/Js/script.js
+++ assets/Js/script.js
@@ -1,23 +1,35 @@
 let budget = 2000;
-document.getElementById('initialBudget').innerHTML = budget + '$';
+let display = document.getElementById('initialBudget').innerHTML = budget + '€';
 
-let appledevices = [500, 300, 200, 100]
+
+let appledevices = [900, 700, 500, 300, 200, 100]
 
 for (let i = 0; i < appledevices.length; i++) {
-    document.getElementById('container').innerHTML += `<button onclick="sub(${appledevices[i]})"> $${appledevices[i]}</button>`
+    document.getElementById('container').innerHTML += `<button onclick="sub(${appledevices[i]})"> €${appledevices[i]}</button>`
 }
 
 function sub (price) {
 budget -= price
-document.getElementById('initialBudget').innerHTML = budget + '$';
+document.getElementById('initialBudget').innerHTML = budget + '€';
 
 if (budget <= 1000)  {
-    // document.getElementById('initialBudget').innerHTML = 'half budget'
-    alert('half budget')
+    document.getElementById('halfBudget').innerHTML = 'HALF BUDGET' 
+    document.getElementById('halfBudget').style.backgroundColor = 'yellow';
+
+} 
+
+if (budget <=500 ) {
+    document.getElementById('halfBudget').innerHTML = 'ALERT' 
+    document.getElementById('halfBudget').style.backgroundColor = 'red';
 }
 
-if (budget = 0)  {
-    // document.getElementById('initialBudget').innerHTML = 'You are poor'
-    alert('You are poor')
+if (budget == 0 ) {
+    document.getElementById('halfBudget').innerHTML = 'YOU ARE POOR' 
+    document.getElementById('halfBudget').style.backgroundColor = 'green';
+}
+
+if (budget < 0 ) {
+    document.getElementById('halfBudget').innerHTML = 'WTF MAN' 
+    document.getElementById('halfBudget').style.backgroundColor = 'orange';
 }
 }
